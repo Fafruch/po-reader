56ff1c6563f63875410425ee99999b7004dce7f1
Fafruch <fafruszek@gmail.com>
2017-12-14 17:57:06 +0100

    Add support for 2 files at the same moment in Main

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
-            if(args.length < 2) {
+            if (args.length < 2) {
                 throw new Error("You passed too few arguments!");
             } else if (args.length < 3 && args[1].equals("-e")) {
                 throw new Error("You passed too few arguments for -e option!");
@@ -20,36 +20,33 @@ public class Main {
                 storedFile.add(line);
             }
 
-            UokikNormalizer uokikNormalizer = new UokikNormalizer();
-            storedFile = uokikNormalizer.cleanFile(storedFile);
-            storedFile = uokikNormalizer.moveUstepsToNewLine(storedFile);
-            storedFile = uokikNormalizer.connectLines(storedFile);
+            if (args[0].equals("konstytucja.txt")) {
+                KonstNormalizer konstNormalizer = new KonstNormalizer();
+                storedFile = konstNormalizer.cleanFile(storedFile);
+                storedFile = konstNormalizer.connectLines(storedFile);
 
-            for(String lineToPrint : storedFile) {
-                System.out.println(lineToPrint);
+                KonstParser konstParser = new KonstParser(storedFile);
+                Node dataTree = konstParser.parseToTree(emptyDataTree);
+
+                KonstPrinter konstPrinter = new KonstPrinter(args);
+                konstPrinter.print(dataTree);
+            } else if (args[0].equals("uokik.txt")) {
+                UokikNormalizer uokikNormalizer = new UokikNormalizer();
+                storedFile = uokikNormalizer.cleanFile(storedFile);
+                storedFile = uokikNormalizer.moveUstepsToNewLine(storedFile);
+                storedFile = uokikNormalizer.connectLines(storedFile);
+
+                UokikParser uokikParser = new UokikParser(storedFile);
+                Node dataTree = uokikParser.parseToTree(emptyDataTree);
+
+                UokikPrinter uokikPrinter = new UokikPrinter(args);
+                uokikPrinter.print(dataTree);
+            } else {
+                throw new Error("I can't parse this file. Sorry. : (");
             }
 
-            UokikParser uokikParser = new UokikParser(storedFile);
-            Node dataTree = uokikParser.parseToTree(emptyDataTree);
-
-            UokikPrinter uokikPrinter = new UokikPrinter(args);
-            uokikPrinter.print(dataTree);
-
-            /*for(int i = 0; i < storedFile.size(); i++) {
-                System.out.println(i + "-> " + storedFile.get(i));
-            }
-            KonstNormalizer konstNormalizer = new KonstNormalizer();
-            storedFile = konstNormalizer.cleanFile(storedFile);
-            storedFile = konstNormalizer.connectLines(storedFile);
-
-            KonstParser konstParser = new KonstParser(storedFile);
-            Node dataTree = konstParser.parseToTree(emptyDataTree);
-
-            KonstPrinter konstPrinter = new KonstPrinter(args);
-            konstPrinter.print(dataTree);*/
-
-        } catch(IOException | Error ex) {
-            if(ex instanceof IOException) {
+        } catch (IOException | Error ex) {
+            if (ex instanceof IOException) {
                 System.out.println("Wrong path! Could not open file.");
             } else {
                 System.out.println(ex.getMessage());
