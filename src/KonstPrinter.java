5ff1bb7a63e6ff7a29d0ef1e7b94fbd29841f612
Fafruch <fafruszek@gmail.com>
2017-12-16 21:56:56 +0100

    Use artykuly static list in KonstPrinter

--- src/KonstPrinter.java
+++ src/KonstPrinter.java
@@ -39,45 +39,46 @@ public class KonstPrinter extends AbstractPrinter {
             printNodeChildren(element);
         } else if(args[2].matches("^art.\\d+$")) {
             String artykul = args[2].substring(4);
-            int artykulIndex = Integer.parseInt(artykul);
+            int artykulIndex = Integer.parseInt(artykul) - 1;
 
-            if(artykulIndex < 1 || artykulIndex > 243) {
+            if(artykulIndex < 0 || artykulIndex > Node.getArtykuly().size() - 1) {
                 throw new Error("Nie ma takiego artykulu!");
             }
 
-            printArtykulyBetween(root, artykulIndex, artykulIndex);
+            printArtykulyBetween(artykulIndex, artykulIndex);
         } else if(args[2].matches("^art.\\d+-\\d+$")) {
             int indexOfDash = args[2].indexOf('-');
 
             String firstArtykul = args[2].substring(4, indexOfDash);
             String lastArtykul = args[2].substring(indexOfDash + 1);
 
-            int firstArtykulIndex = Integer.parseInt(firstArtykul);
-            int lastArtykulIndex = Integer.parseInt(lastArtykul);
+            int firstArtykulIndex = Integer.parseInt(firstArtykul) - 1;
+            int lastArtykulIndex = Integer.parseInt(lastArtykul) - 1;
 
             if(firstArtykulIndex > lastArtykulIndex) {
                 throw new Error("Niepoprawny zakres artykulow!");
             }
 
-            if(firstArtykulIndex < 1 || firstArtykulIndex > 243 || lastArtykulIndex < 1 || lastArtykulIndex > 243) {
+            if(firstArtykulIndex < 0 || firstArtykulIndex > Node.getArtykuly().size() - 1
+                    || lastArtykulIndex < 0 || lastArtykulIndex > Node.getArtykuly().size() - 1) {
                 throw new Error("Nie ma takiego artykulu!");
             }
 
-            printArtykulyBetween(root, firstArtykulIndex, lastArtykulIndex);
+            printArtykulyBetween(firstArtykulIndex, lastArtykulIndex);
         } else if(args[2].matches("^art.\\d+,ust.\\d+$")) {
             int indexOfComma = args[2].indexOf(',');
 
             String artykul = args[2].substring(4, indexOfComma);
             String ustep = args[2].substring(indexOfComma + 5);
 
-            int artykulIndex = Integer.parseInt(artykul);
+            int artykulIndex = Integer.parseInt(artykul) - 1;
             int ustepIndex = Integer.parseInt(ustep) - 1;
 
-            if(artykulIndex < 1 || artykulIndex > 243) {
+            if(artykulIndex < 0 || artykulIndex > Node.getArtykuly().size() - 1) {
                 throw new Error("Nie ma takiego artykulu!");
             }
 
-            Node artykulNode = getArtykul(root, artykulIndex);
+            Node artykulNode = Node.getArtykuly().get(artykulIndex);
 
             if(artykulNode == null) {
                 throw new Error("Nie znaleziono takiego artykulu!");
@@ -89,6 +90,7 @@ public class KonstPrinter extends AbstractPrinter {
 
             printNodeChildren(artykulNode.getChildren().get(ustepIndex));
         } else if(args[2].matches("^art.\\d+,ust.\\d+,pkt\\d+\\)$")) {
+            String[] argsSplit = args[2].split(",");
             int firstIndexOfComma = args[2].indexOf(',');
             int lastIndexOfComma = args[2].lastIndexOf(',');
             int indexOfParenthesis = args[2].lastIndexOf(')');
@@ -97,15 +99,26 @@ public class KonstPrinter extends AbstractPrinter {
             String ustep = args[2].substring(firstIndexOfComma + 5, lastIndexOfComma);
             String punkt = args[2].substring(lastIndexOfComma + 4, indexOfParenthesis);
 
-            int artykulIndex = Integer.parseInt(artykul);
+            int artykulIndex = Integer.parseInt(artykul) - 1;
             int ustepIndex = Integer.parseInt(ustep) - 1;
             int punktIndex = Integer.parseInt(punkt) - 1;
 
-            if(artykulIndex < 1 || artykulIndex > 243) {
+            for(String part : argsSplit) {
+                System.out.println(part);
+            }
+            System.out.println(artykul);
+            System.out.println(artykulIndex);
+            System.out.println(ustep);
+            System.out.println(ustepIndex);
+            System.out.println(punkt);
+            System.out.println(punktIndex);
+
+
+            if(artykulIndex < 0 || artykulIndex > Node.getArtykuly().size() - 1) {
                 throw new Error("Nie ma takiego artykulu!");
             }
 
-            Node artykulNode = getArtykul(root, artykulIndex);
+            Node artykulNode = Node.getArtykuly().get(artykulIndex);
 
             if(artykulNode == null) {
                 throw new Error("Nie znaleziono takiego artykulu!");
@@ -123,49 +136,13 @@ public class KonstPrinter extends AbstractPrinter {
         }
     }
 
-    private void printArtykulyBetween(Node node, int firstArtykulIndex, int lastArtykulIndex) {
-        if(node.getDepth() == 3) {
-            String data = node.getData();
-            int lastIndexOfDot = data.lastIndexOf('.');
-            String artykul = node.getData().substring(5, lastIndexOfDot);
+    private void printArtykulyBetween(int firstArtykulIndex, int lastArtykulIndex) {
+        for(int i = 0; i < Node.getArtykuly().size(); i++) {
+            Node artykulNode = Node.getArtykuly().get(i);
 
-            int artykulIndex = Integer.parseInt(artykul);
-
-            if(firstArtykulIndex <= artykulIndex && artykulIndex <= lastArtykulIndex) {
-                printNodeChildren(node);
-            }
-
-            return;
-        }
-
-        for(Node child : node.getChildren()) {
-            printArtykulyBetween(child, firstArtykulIndex, lastArtykulIndex);
-        }
-    }
-
-    private Node getArtykul(Node node, int index) {
-        if(node.getDepth() == 3) {
-            String data = node.getData();
-            int lastIndexOfDot = data.lastIndexOf('.');
-            String artykul = node.getData().substring(5, lastIndexOfDot);
-
-            int artykulIndex = Integer.parseInt(artykul);
-
-            if(artykulIndex == index) {
-                return node;
-            } else {
-                return null;
+            if(firstArtykulIndex <= i && i <= lastArtykulIndex) {
+                printNodeChildren(artykulNode);
             }
         }
-
-        for(Node child : node.getChildren()) {
-            Node result = getArtykul(child, index);
-
-            if(result != null) {
-                return result;
-            }
-        }
-
-        return null;
     }
 }
