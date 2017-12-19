f93eff06eee1398950e09e686b2a56665328bee3
Fafruch <fafruszek@gmail.com>
2017-12-19 09:48:48 +0100

    Add functions in UokikPrinter

--- src/UokikPrinter.java
+++ src/UokikPrinter.java
@@ -7,155 +7,32 @@ public class UokikPrinter extends AbstractPrinter {
         KonstNormalizer konstNormalizer = new KonstNormalizer();
         config = konstNormalizer.normalizeString(config);
 
-        // np. Dział IIIA
         if(config.matches("^dzia[łl]\\w{1,4}$")) {
-            int indexOfComma = config.indexOf(',');
-            String dzial = config.substring(0, indexOfComma);
+            // np. Dział IIIA
+            printDzial();
 
-            Node dzialNode = findNodeAtDepth(root, dzial, 1);
-
-            if(dzialNode == null) {
-                throw new NotFoundException("Nie ma takiego dzialu!");
-            }
-
-            printNodeAndItsChildren(dzialNode);
-
-            // np. Dział IIIA, rozdział 2
         } else if(config.matches("^dzia[łl]\\w{1,4},rozdzia[łl]\\d+$")) {
-            int indexOfComma = config.indexOf(',');
-            String dzial = config.substring(0, indexOfComma);
-            String rozdzial = config.substring(indexOfComma + 1);
+            // np. Dział IIIA, rozdział 2
+            printRozdzial();
 
-            Node dzialNode = findNodeAtDepth(root, dzial, 1);
-
-            if(dzialNode == null) {
-                throw new NotFoundException("Nie ma takiego dzialu!");
-            }
-
-            Node rozdzialNode = findNodeAtDepth(dzialNode, rozdzial, 2);
-
-            if(rozdzialNode == null) {
-                throw new NotFoundException("Nie ma takiego rozdzialu!");
-            }
-
-            printNodeAndItsChildren(rozdzialNode);
-
-            // np. Art. 119j
         } else if(config.matches("^art\\.\\d{1,3}[a-z]{0,3}(\\.)?$")) {
-            String artykul = config;
+            // np. Art. 119j
+            printArtykul();
 
-            Node artykulNode = findNodeAtDepth(root, artykul, 3);
-
-            if(artykulNode == null) {
-                throw new NotFoundException("Nie ma takiego artykulu!");
-            }
-
-            printNodeAndItsChildren(artykulNode);
-
-            // np. Art. 119j-121b
         } else if(config.matches("^art\\.\\d{1,3}[a-z]{0,3}(\\.)?-\\d{1,3}[a-z]{0,3}(\\.)?$")) {
-            int indexOfDash = config.indexOf('-');
+            // np. Art. 119j-121b
+            printArtykuly();
 
-            String firstArtykul = config.substring(0, indexOfDash);
-            String lastArtykul = "art." + config.substring(indexOfDash + 1);
-
-            /*System.out.println(firstArtykul);
-            System.out.println(lastArtykul);*/
-
-            if(firstArtykul.compareTo(lastArtykul) > 0) {
-                throw new IllegalArgumentException("Niepoprawny zakres artykulow!");
-            }
-
-            printArtykulyBetween(firstArtykul, lastArtykul);
-
-            // np. Art. 30a, ust. 2a.
         } else if(config.matches("^art\\.\\d{1,3}[a-z]{0,3}(\\.)?,ust\\.\\d{1,3}[a-z]{0,3}\\.$")) {
-            int indexOfComma = config.indexOf(',');
-
-            String artykul = config.substring(0, indexOfComma);
-            String ustep = config.substring(indexOfComma + 5);
-
-            Node artykulNode = findNodeAtDepth(root, artykul, 3);
-
-            if(artykulNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego artykulu!");
-            }
-
-            Node ustepNode = findNodeAtDepth(artykulNode, ustep, 4);
-
-            if(ustepNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego ustepu!");
-            }
-
-            printNodeAndItsChildren(ustepNode);
-
-            // np. Art. 30a, ust. 2a., pkt 1a)
+            // np. Art. 30a, ust. 2a.
+            printUstep();
         } else if(config.matches("^art\\.\\d{1,3}[a-z]{0,3}(\\.)?,ust\\.\\d{1,3}[a-z]{0,3}\\.,pkt\\d{1,3}[a-z]{0,3}\\)$")) {
-            int firstIndexOfComma = config.indexOf(',');
-            int lastIndexOfComma = config.lastIndexOf(',');
-            int indexOfParenthesis = config.lastIndexOf(')');
+            // np. Art. 30a, ust. 2a., pkt 1a)
+            printPunkt();
 
-            String artykul = config.substring(0, firstIndexOfComma);
-            String ustep = config.substring(firstIndexOfComma + 5, lastIndexOfComma);
-            String punkt = config.substring(lastIndexOfComma + 4, indexOfParenthesis);
-
-            Node artykulNode = findNodeAtDepth(root, artykul, 3);
-
-            if(artykulNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego artykulu!");
-            }
-
-            Node ustepNode = findNodeAtDepth(artykulNode, ustep, 4);
-
-            if(ustepNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego ustepu!");
-            }
-
-            Node punktNode = findNodeAtDepth(ustepNode, punkt, 5);
-
-            if(punktNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego punktu!");
-            }
-
-            printNodeAndItsChildren(punktNode);
-
-            // np. Art. 30a, ust. 2a., pkt 1a), lit. b)
         } else if(config.matches("^art\\.\\d{1,3}[a-z]{0,3}(\\.)?,ust\\.\\d{1,3}[a-z]{0,3}\\.,pkt\\d{1,3}[a-z]{0,3}\\),lit.[a-z]{1,3}\\)$")) {
-            int firstIndexOfComma = config.indexOf(',');
-            int secondIndexOfComma = firstIndexOfComma + config.substring(firstIndexOfComma + 1).indexOf(',');
-            int firstIndexOfParenthesis = config.indexOf(')');
-            int lastIndexOfParenthesis = config.lastIndexOf(')');
-
-            String artykul = config.substring(0, firstIndexOfComma);
-            String ustep = config.substring(firstIndexOfComma + 5, secondIndexOfComma);
-            String punkt = config.substring(secondIndexOfComma + 5, firstIndexOfParenthesis);
-            String litera = config.substring(firstIndexOfParenthesis + 6, lastIndexOfParenthesis);
-
-            Node artykulNode = findNodeAtDepth(root, artykul, 3);
-
-            if (artykulNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego artykulu!");
-            }
-
-            Node ustepNode = findNodeAtDepth(artykulNode, ustep, 4);
-
-            if (ustepNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego ustepu!");
-            }
-
-            Node punktNode = findNodeAtDepth(ustepNode, punkt, 5);
-
-            if (punktNode == null) {
-                throw new NotFoundException("Nie znaleziono takiego punktu!");
-            }
-
-            Node literaNode = findNodeAtDepth(punktNode, litera, 6);
-
-            if (literaNode == null) {
-                throw new NotFoundException("Nie znaleziono takiej litery!");
-            }
-
-            printNodeAndItsChildren(literaNode);
+            // np. Art. 30a, ust. 2a., pkt 1a), lit. b)
+            printLitera();
         }
     }
 
@@ -170,14 +47,6 @@ public class UokikPrinter extends AbstractPrinter {
             normalizedData = uokikNormalizer.normalizeString(data);
             normalizedData = uokikNormalizer.removeLastChar(normalizedData);
 
-            /*System.out.println("");
-            System.out.println("1. " + firstArtykulName + ", length: " + firstArtykulName.length());
-            System.out.println("2. " + (firstArtykulName.compareTo(normalizedData) <= 0));
-            System.out.println("3. " + normalizedData + ", length: " + normalizedData.length());
-            System.out.println("4. " + (normalizedData.compareTo(lastArtykulName) <= 0));
-            System.out.println("5. " + lastArtykulName + ", length: " + lastArtykulName.length());
-            System.out.println("");*/
-
             if(firstArtykulName.compareTo(normalizedData) <= 0
                     && normalizedData.compareTo(lastArtykulName) <= 0
                     && normalizedData.length() >= firstArtykulName.length()
@@ -187,6 +56,155 @@ public class UokikPrinter extends AbstractPrinter {
             }
         }
     }
+
+    private void printDzial() throws NotFoundException {
+        // np. Dział IIIA
+        int indexOfComma = config.indexOf(',');
+        String dzial = config.substring(0, indexOfComma);
+
+        Node dzialNode = findNodeAtDepth(root, dzial, 1);
+
+        if(dzialNode == null) {
+            throw new NotFoundException("Nie ma takiego dzialu!");
+        }
+
+        printNodeAndItsChildren(dzialNode);
+    }
+
+    private void printRozdzial() throws NotFoundException {
+        int indexOfComma = config.indexOf(',');
+        String dzial = config.substring(0, indexOfComma);
+        String rozdzial = config.substring(indexOfComma + 1);
+
+        Node dzialNode = findNodeAtDepth(root, dzial, 1);
+
+        if(dzialNode == null) {
+            throw new NotFoundException("Nie ma takiego dzialu!");
+        }
+
+        Node rozdzialNode = findNodeAtDepth(dzialNode, rozdzial, 2);
+
+        if(rozdzialNode == null) {
+            throw new NotFoundException("Nie ma takiego rozdzialu!");
+        }
+
+        printNodeAndItsChildren(rozdzialNode);
+    }
+
+    private void printArtykul() throws NotFoundException {
+        String artykul = config;
+
+        Node artykulNode = findNodeAtDepth(root, artykul, 3);
+
+        if(artykulNode == null) {
+            throw new NotFoundException("Nie ma takiego artykulu!");
+        }
+
+        printNodeAndItsChildren(artykulNode);
+    }
+
+    private void printArtykuly() throws NotFoundException, IllegalArgumentException {
+        int indexOfDash = config.indexOf('-');
+
+        String firstArtykul = config.substring(0, indexOfDash);
+        String lastArtykul = "art." + config.substring(indexOfDash + 1);
+
+        if(firstArtykul.compareTo(lastArtykul) > 0) {
+            throw new IllegalArgumentException("Niepoprawny zakres artykulow!");
+        }
+
+        printArtykulyBetween(firstArtykul, lastArtykul);
+    }
+
+    private void printUstep() throws NotFoundException {
+        int indexOfComma = config.indexOf(',');
+
+        String artykul = config.substring(0, indexOfComma);
+        String ustep = config.substring(indexOfComma + 5);
+
+        Node artykulNode = findNodeAtDepth(root, artykul, 3);
+
+        if(artykulNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego artykulu!");
+        }
+
+        Node ustepNode = findNodeAtDepth(artykulNode, ustep, 4);
+
+        if(ustepNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego ustepu!");
+        }
+
+        printNodeAndItsChildren(ustepNode);
+    }
+
+    private void printPunkt() throws NotFoundException {
+        int firstIndexOfComma = config.indexOf(',');
+        int lastIndexOfComma = config.lastIndexOf(',');
+        int indexOfParenthesis = config.lastIndexOf(')');
+
+        String artykul = config.substring(0, firstIndexOfComma);
+        String ustep = config.substring(firstIndexOfComma + 5, lastIndexOfComma);
+        String punkt = config.substring(lastIndexOfComma + 4, indexOfParenthesis);
+
+        Node artykulNode = findNodeAtDepth(root, artykul, 3);
+
+        if(artykulNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego artykulu!");
+        }
+
+        Node ustepNode = findNodeAtDepth(artykulNode, ustep, 4);
+
+        if(ustepNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego ustepu!");
+        }
+
+        Node punktNode = findNodeAtDepth(ustepNode, punkt, 5);
+
+        if(punktNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego punktu!");
+        }
+
+        printNodeAndItsChildren(punktNode);
+    }
+
+    private void printLitera() throws NotFoundException {
+        int firstIndexOfComma = config.indexOf(',');
+        int secondIndexOfComma = firstIndexOfComma + config.substring(firstIndexOfComma + 1).indexOf(',');
+        int firstIndexOfParenthesis = config.indexOf(')');
+        int lastIndexOfParenthesis = config.lastIndexOf(')');
+
+        String artykul = config.substring(0, firstIndexOfComma);
+        String ustep = config.substring(firstIndexOfComma + 5, secondIndexOfComma);
+        String punkt = config.substring(secondIndexOfComma + 5, firstIndexOfParenthesis);
+        String litera = config.substring(firstIndexOfParenthesis + 6, lastIndexOfParenthesis);
+
+        Node artykulNode = findNodeAtDepth(root, artykul, 3);
+
+        if (artykulNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego artykulu!");
+        }
+
+        Node ustepNode = findNodeAtDepth(artykulNode, ustep, 4);
+
+        if (ustepNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego ustepu!");
+        }
+
+        Node punktNode = findNodeAtDepth(ustepNode, punkt, 5);
+
+        if (punktNode == null) {
+            throw new NotFoundException("Nie znaleziono takiego punktu!");
+        }
+
+        Node literaNode = findNodeAtDepth(punktNode, litera, 6);
+
+        if (literaNode == null) {
+            throw new NotFoundException("Nie znaleziono takiej litery!");
+        }
+
+        printNodeAndItsChildren(literaNode);
+    }
+
 
     private Node findNodeAtDepth(Node root, String name, int depth) {
         if(root.getDepth() == depth) {
